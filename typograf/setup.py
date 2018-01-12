6f5a6cabd21d9814679e1efa727bf43f306f89ef
Melfi Silver <s.pirogov@designdepot.ru>
2018-01-12 15:27:07 +0300

    bump version

--- typograf/setup.py
+++ typograf/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='typograf',
-    version='0.1.0',
+    version='0.1.1',
     description=u'Типограф студии Лебедева',
     url='https://github.com/diveru4i/typograf',
     author='Melfi Silver',
